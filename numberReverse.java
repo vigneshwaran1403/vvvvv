7e64dbab6e8f4a646a3131cdc94f67faa8285d3b
vigneshwaran1403 <113986227+vigneshwaran1403@users.noreply.github.com>
2023-09-13 14:50:31 +0530

    Add files via upload

---
+++ numberReverse.java
@@ -0,0 +1,25 @@
+package leanering;
+import java.util.Scanner;
+public class numberReverse {
+    public static void main(String []args)
+    {
+    	Scanner s=new Scanner(System.in);
+    	System.out.println("Enter the number");
+    	int number=s.nextInt();
+    	int temp=number;
+    	int total=0;
+    	while(number>0)
+    	{
+    		int lastvalue=number%10;
+    		 total=total*10+lastvalue;
+    		  number=number/10;
+    		
+    	}
+    	if(temp==total)
+    	System.out.println("polindram");
+    	
+    	else
+    	System.out.println("not polindram");
+    	
+    }
+}
